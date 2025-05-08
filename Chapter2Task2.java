a8702ee593bcd52ef9901cc11d708924cb6bd56c
rhavalencia <70908464+rhavalencia@users.noreply.github.com>
2025-05-08 16:42:18 +0000

    Added Chapter 2 task 2

---
+++ Chapter2Task2.java
@@ -0,0 +1,29 @@
+public class Chapter2Task2 implements Activities{
+
+
+
+    @Override
+    public void chapterNumber() {
+        char H = 'H';
+        char three = '3';
+        char one1 = '1';
+        char one2 = '1';
+        char zero = '0';
+        char space = ' ';
+        char w = 'w';
+        char zero2 = '0';
+        char r = 'r';
+        char one3 = '1';
+        char d = 'd';
+        char space2 = ' ';
+        float twoPointZero = 2.0f;
+        char space3 = ' ';
+        boolean boolValue = true;
+
+        String result = "" + H + three + one1 + one2 + zero + space + w + zero2 + r + one3 + d + space2 + twoPointZero
+                + space3 + boolValue;
+
+        System.out.println(result);
+    }
+
+}
