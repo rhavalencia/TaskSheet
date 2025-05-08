f222d468c93b5d063260cc5838e194e1b2b840a0
rhavalencia <70908464+rhavalencia@users.noreply.github.com>
2025-05-08 16:52:20 +0000

    updated task2

--- Chapter2Task2.java
+++ Chapter2Task2.java
@@ -1,29 +1,21 @@
-public class Chapter2Task2 implements Activities{
-
-
-
-    @Override
-    public void chapterNumber() {
-        char H = 'H';
-        char three = '3';
-        char one1 = '1';
-        char one2 = '1';
-        char zero = '0';
-        char space = ' ';
-        char w = 'w';
-        char zero2 = '0';
-        char r = 'r';
-        char one3 = '1';
-        char d = 'd';
-        char space2 = ' ';
-        float twoPointZero = 2.0f;
-        char space3 = ' ';
-        boolean boolValue = true;
-
-        String result = "" + H + three + one1 + one2 + zero + space + w + zero2 + r + one3 + d + space2 + twoPointZero
-                + space3 + boolValue;
-
-        System.out.println(result);
-    }
-
+public class Chapter2Task2 {
+        public static void main(String[] args) {
+            char H = 'H';
+            char three = '3';
+            char one1 = '1';
+            char one2 = '1';
+            char zero = '0';
+            char w = 'w';
+            char zero2 = '0';
+            char r = 'r';
+            char one3 = '1';
+            char d = 'd';
+            float twoPointZero = 2.0f;
+            char space3 = ' ';
+            boolean boolValue = true;
+    
+            String result = "" + H + three + one1 + one2 + zero + " " + w + zero2 + r + one3 + d + " " + twoPointZero + space3 + boolValue;
+    
+            System.out.println(result);
+        }
 }
